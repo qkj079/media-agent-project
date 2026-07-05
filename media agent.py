434b51a8a4fb86c3b77b41505c3e635640ee6bf6
qkj079 <3067582164@qq.com>
2026-07-05 17:30:06 +0800

    Update media agent.py

--- media agent.py
+++ media agent.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_openai import ChatOpenAI
 from langchain.agents import AgentExecutor
-from langchain.agents.react.base import create_react_agent
+from langchain.agents import create_react_agent
 from langchain_core.tools import Tool
 from langchain.memory import ConversationBufferMemory
 import requests 
